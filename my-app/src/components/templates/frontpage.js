7a0dfd514f7af586a05aac9f375362880b5955e9
havocthehobbit <havocthehobbit@gmail.com>
2022-01-10 17:45:50 +0200

    quick frontpage fix , for git pull issue

--- my-app/src/components/templates/frontpage.js
+++ my-app/src/components/templates/frontpage.js
@@ -1,7 +1,7 @@
 import { useEffect, useState,Component} from "react"
 import _ from "lodash";
 import $gl from "../common/global";
-
+import Login ,{} from "../common/login";
 
 
 
@@ -23,6 +23,23 @@ export const Frontpage=(props)=>{
     return (
         <div>
             <Login/>
+
+            {/* example of manipulating login screen or front page
+                <h2>Front Page</h2>
+                <Login 
+                    style={{ width : 50,
+                        position: "relative", top : 100}} 
+                        theme={props.theme} 
+                        namestuff={{ name : "frontpage stuff"}
+                    } 
+
+                    style_account={{ margin : 0,position : "relative", color : "green", height : 15 ,background : 10}}
+                    style_password={{ margin : 0,position : "relative", color : "green", height : 15 ,background : 10}}
+                
+                />
+            
+            
+            */}
         </div>
     )
 }
